be1d8c3ac63047404669cb213fc8e6427ba93e1b
isabelfecker <67733168+isabelfecker@users.noreply.github.com>
2020-07-14 10:16:18 +0200

    digital

--- SF/360/data.js
+++ SF/360/data.js
@@ -1,8 +1,49 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-start",
-      "name": "Start",
+      "id": "0-auenansicht",
+      "name": "Außenansicht",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24265206476023593,
+          "pitch": 0.31662563628662,
+          "rotation": 11.780972450961727,
+          "target": "1-eingangsbereich"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6352914359117285,
+          "pitch": -0.24330563899439994,
+          "title": "TheNewLearning",
+          "text": "<p class=\"western\" style=\"font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 300; font-size: 13px; line-height: 13px; font-family: Helvetica, Arial, sans-serif; margin-right: -1.79cm; margin-bottom: 0cm;\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\">Büffeln, stillsitzen und schwere Bücher mit sich schleppen. Unsere Generation ist zwar schon längst im digitalen Zeitalter angekommen, nur blieben die Lernmethoden in den Schulen bis heute noch größtenteils im Buchdruckzeithalter stecken. In der Schule der Zukunft sollen neue Maßstäbe gesetzt werden und Möglichkeiten geben</font></font><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\">, um das Lernen attraktiver und individueller zu gestalten.</font></font></p><div><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-eingangsbereich",
+      "name": "Eingangsbereich",
       "levels": [
         {
           "tileSize": 256,
@@ -29,16 +70,16 @@ var APP_DATA = {
           "yaw": 0.6663692446686724,
           "pitch": -0.07848267300026279,
           "rotation": 11.780972450961727,
-          "target": "1-01"
+          "target": "2-ebene-1"
+        },
+        {
+          "yaw": 2.284329207104621,
+          "pitch": 0.25312505821631603,
+          "rotation": 13.351768777756625,
+          "target": "0-auenansicht"
         }
       ],
       "infoHotspots": [
-        {
-          "yaw": 0.3110652023754721,
-          "pitch": 0.2393862875649404,
-          "title": "The New Learning",
-          "text": "<p class=\"western\" style=\"margin-right: -1.79cm; margin-bottom: 0cm; line-height: 100%\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\">Büffeln,\nstillsitzen und schwere Bücher mit sich schleppen. Unsere Generation\nist zwar schon längst im digitalen Zeitalter angekommen, nur blieben\ndie Lernmethoden in den Schulen bis heute noch größtenteils im\nBuchdruckzeithalter stecken. In der Schule der Zukunft sollen neue\nMaßstäbe gesetzt und Möglichkeiten </font></font><font color=\"#000000\" style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\">gegeben\nwerden</span></font></font></font><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\">,\num das Lernen attraktiver und individueller zu gestalten.</font></font></p><p class=\"western\" style=\"margin-right: -1.79cm; margin-bottom: 0cm; line-height: 100%\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\"><br></font></font></p>"
-        },
         {
           "yaw": -0.19254445324613023,
           "pitch": -0.041011329067458746,
@@ -48,8 +89,8 @@ var APP_DATA = {
       ]
     },
     {
-      "id": "1-01",
-      "name": "01",
+      "id": "2-ebene-1",
+      "name": "Ebene 1",
       "levels": [
         {
           "tileSize": 256,
@@ -76,13 +117,13 @@ var APP_DATA = {
           "yaw": 2.0070523075506417,
           "pitch": -0.11967833708863473,
           "rotation": 0.7853981633974483,
-          "target": "2-02"
+          "target": "3-ebene-2"
         },
         {
           "yaw": -2.4305803522313774,
           "pitch": -0.2005660180168185,
           "rotation": 10.995574287564278,
-          "target": "3-03"
+          "target": "4-ebene-3"
         }
       ],
       "infoHotspots": [
@@ -107,8 +148,8 @@ var APP_DATA = {
       ]
     },
     {
-      "id": "2-02",
-      "name": "02",
+      "id": "3-ebene-2",
+      "name": "Ebene 2",
       "levels": [
         {
           "tileSize": 256,
@@ -135,7 +176,7 @@ var APP_DATA = {
           "yaw": 2.689055903859673,
           "pitch": -0.018979984592249366,
           "rotation": 10.995574287564278,
-          "target": "1-01"
+          "target": "2-ebene-1"
         }
       ],
       "infoHotspots": [
@@ -154,8 +195,8 @@ var APP_DATA = {
       ]
     },
     {
-      "id": "3-03",
-      "name": "03",
+      "id": "4-ebene-3",
+      "name": "Ebene 3",
       "levels": [
         {
           "tileSize": 256,
@@ -182,7 +223,7 @@ var APP_DATA = {
           "yaw": 0.5387532498967857,
           "pitch": 0.0921013621293234,
           "rotation": 3.141592653589793,
-          "target": "0-start"
+          "target": "1-eingangsbereich"
         }
       ],
       "infoHotspots": []
