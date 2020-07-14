0761d0b137317c924f9ad4425205c656c476008b
isabelfecker <67733168+isabelfecker@users.noreply.github.com>
2020-07-14 09:38:47 +0200

    digital

---
+++ SF/360/data.js
@@ -0,0 +1,198 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6663692446686724,
+          "pitch": -0.07848267300026279,
+          "rotation": 11.780972450961727,
+          "target": "1-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3110652023754721,
+          "pitch": 0.2393862875649404,
+          "title": "The New Learning",
+          "text": "<p class=\"western\" style=\"margin-right: -1.79cm; margin-bottom: 0cm; line-height: 100%\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\">Büffeln,\nstillsitzen und schwere Bücher mit sich schleppen. Unsere Generation\nist zwar schon längst im digitalen Zeitalter angekommen, nur blieben\ndie Lernmethoden in den Schulen bis heute noch größtenteils im\nBuchdruckzeithalter stecken. In der Schule der Zukunft sollen neue\nMaßstäbe gesetzt und Möglichkeiten </font></font><font color=\"#000000\" style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\">gegeben\nwerden</span></font></font></font><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\">,\num das Lernen attraktiver und individueller zu gestalten.</font></font></p><p class=\"western\" style=\"margin-right: -1.79cm; margin-bottom: 0cm; line-height: 100%\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt\"><br></font></font></p>"
+        },
+        {
+          "yaw": -0.19254445324613023,
+          "pitch": -0.041011329067458746,
+          "title": "''button''",
+          "text": "Das neuen digitalen Lernwerkzeug ''button'' soll das Lernen in Zukunft einfacher und entspannter gestalten.<div>Der Datenspeicher, der gerade einmal die Grölße einer zwei Euro Münze hat, ersetzt die veralteten schwere Bücher und die Unmengen an Ordnern und Hefte.</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0070523075506417,
+          "pitch": -0.11967833708863473,
+          "rotation": 0.7853981633974483,
+          "target": "2-02"
+        },
+        {
+          "yaw": -2.4305803522313774,
+          "pitch": -0.2005660180168185,
+          "rotation": 10.995574287564278,
+          "target": "3-03"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.169283559299682,
+          "pitch": 0.07547999896892321,
+          "title": "Watering Hole",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.22638511584527343,
+          "pitch": 0.4917916037640566,
+          "title": "Mountain Top",
+          "text": "Auf einer hinter der Fassade liegenden Ebene ist es möglich große zweidimensionale Projektionen abzubilden. Wird der ''button'' an dieser angedockt,so können die darauf gespeicherten Informationen wiedergegeben werden und damit interagiert werden."
+        },
+        {
+          "yaw": 1.0795576532806734,
+          "pitch": 0.038857079132533556,
+          "title": "Wandlungsfähige Fassade",
+          "text": "Die an der Fassade angebrachte Faltkonstruktion kann nach Bedarf ein- und ausgefahren werden. Durch die Beweglichkeit des Sonnenschutzes kann das Gebäude offener oder geschlossener für die Außenwelt gemacht werden."
+        }
+      ]
+    },
+    {
+      "id": "2-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.689055903859673,
+          "pitch": -0.018979984592249366,
+          "rotation": 10.995574287564278,
+          "target": "1-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.023216042738747333,
+          "pitch": 0.34280421293277463,
+          "title": "Cave",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9153546202187535,
+          "pitch": -0.039951744361395924,
+          "title": "Campfire",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5387532498967857,
+          "pitch": 0.0921013621293234,
+          "rotation": 3.141592653589793,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
