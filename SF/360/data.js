487b260b19821fc9cf02fc4e2b0b6896fc638c7f
isabelfecker <67733168+isabelfecker@users.noreply.github.com>
2020-07-17 17:54:11 +0200

    digital

--- SF/360/data.js
+++ SF/360/data.js
@@ -26,18 +26,18 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.24265206476023593,
-          "pitch": 0.31662563628662,
-          "rotation": 11.780972450961727,
+          "yaw": 0.18425019991264158,
+          "pitch": 0.5076899065695031,
+          "rotation": 6.283185307179586,
           "target": "1-eingangsbereich"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.5765733308254539,
-          "pitch": 0.35522869658839085,
-          "title": "TheNewLearning",
-          "text": "<p class=\"western\" style=\"font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 300; font-size: 13px; line-height: 13px; font-family: Helvetica, Arial, sans-serif; margin-right: -1.79cm; margin-bottom: 0cm;\"><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\">Büffeln, stillsitzen und schwere Bücher mit sich schleppen. Unsere Generation ist zwar schon längst im digitalen Zeitalter angekommen, nur blieben die Lernmethoden in den Schulen bis heute noch größtenteils im Buchdruckzeithalter stecken. In der Schule der Zukunft sollen neue Maßstäbe gesetzt werden und Möglichkeiten geben</font></font><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\">, um das Lernen attraktiver und individueller zu gestalten.</font></font></p><div><font face=\"Arial, sans-serif\" style=\"font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"><font style=\"font-size: 9pt;\"><br></font></font></div>"
+          "yaw": 0.9528126681206182,
+          "pitch": -0.4211624064863422,
+          "title": "The New Learning",
+          "text": "<img src= images/titel.png>"
         }
       ]
     },
@@ -67,89 +67,30 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6663692446686724,
-          "pitch": -0.07848267300026279,
-          "rotation": 11.780972450961727,
-          "target": "2-ebene-1"
-        },
-        {
-          "yaw": 2.284329207104621,
-          "pitch": 0.25312505821631603,
-          "rotation": 13.351768777756625,
-          "target": "0-auenansicht"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.19254445324613023,
-          "pitch": -0.041011329067458746,
-          "title": "''button''",
-          "text": "Das neuen digitalen Lernwerkzeug ''button'' soll das Lernen in Zukunft einfacher und entspannter gestalten.<div>Der Datenspeicher, der gerade einmal die Grölße einer zwei Euro Münze hat, ersetzt die veralteten schwere Bücher und die Unmengen an Ordnern und Hefte.</div>"
-        }
-      ]
-    },
-    {
-      "id": "2-ebene-1",
-      "name": "Ebene 1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.0070523075506417,
-          "pitch": -0.11967833708863473,
+          "yaw": 1.9095747881082845,
+          "pitch": -0.12690464621858055,
           "rotation": 0.7853981633974483,
-          "target": "3-ebene-2"
+          "target": "0-auenansicht"
         },
         {
-          "yaw": -2.4305803522313774,
-          "pitch": -0.2005660180168185,
-          "rotation": 10.995574287564278,
-          "target": "4-ebene-3"
+          "yaw": -0.0751831730107888,
+          "pitch": 0.20198172196669084,
+          "rotation": 18.06415775814132,
+          "target": "2-eg"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -2.169283559299682,
-          "pitch": 0.07547999896892321,
-          "title": "Watering Hole",
-          "text": "Text"
-        },
-        {
-          "yaw": -0.19141041694233962,
-          "pitch": 0.4435199244888839,
-          "title": "Mountain Top",
-          "text": ""
-        },
-        {
-          "yaw": 1.0795576532806734,
-          "pitch": 0.038857079132533556,
-          "title": "Wandlungsfähige Fassade",
-          "text": "Die an der Fassade angebrachte Faltkonstruktion kann nach Bedarf ein- und ausgefahren werden. Durch die Beweglichkeit des Sonnenschutzes kann das Gebäude offener oder geschlossener für die Außenwelt gemacht werden."
+          "yaw": 0.5540821090072185,
+          "pitch": -0.8155077473814902,
+          "title": "Wandlungsfähige Fassade<div><br></div>",
+          "text": "<img src= images/fassade.png>"
         }
       ]
     },
     {
-      "id": "3-ebene-2",
-      "name": "Ebene 2",
+      "id": "2-eg",
+      "name": "EG",
       "levels": [
         {
           "tileSize": 256,
@@ -173,63 +114,181 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.689055903859673,
-          "pitch": -0.018979984592249366,
-          "rotation": 10.995574287564278,
-          "target": "2-ebene-1"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 0.023216042738747333,
-          "pitch": 0.34280421293277463,
-          "title": "Cave",
-          "text": ""
-        },
-        {
-          "yaw": -2.806081394482094,
-          "pitch": 0.012791208784314279,
-          "title": "Campfire",
-          "text": "Text"
-        }
-      ]
-    },
-    {
-      "id": "4-ebene-3",
-      "name": "Ebene 3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.5387532498967857,
-          "pitch": 0.0921013621293234,
-          "rotation": 3.141592653589793,
+          "yaw": 2.437625301937982,
+          "pitch": 0.21456085419846005,
+          "rotation": 13.351768777756625,
           "target": "1-eingangsbereich"
+        },
+        {
+          "yaw": 0.9341522862486702,
+          "pitch": 0.12708223624923676,
+          "rotation": 18.06415775814132,
+          "target": "3-1-og"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.47900390854198704,
+          "pitch": -0.2289650696461205,
+          "title": "''button''",
+          "text": "<img src= images/button_01.png>"
+        },
+        {
+          "yaw": 0.6324303739207142,
+          "pitch": -0.3649060996174036,
+          "title": "Konstruktion",
+          "text": "<img src= images/konstruktion.png>"
+        }
+      ]
+    },
+    {
+      "id": "3-1-og",
+      "name": "1. OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.305159900676821,
+          "pitch": 0.642389067173692,
+          "rotation": 6.283185307179586,
+          "target": "2-eg"
+        },
+        {
+          "yaw": -2.4017020854653293,
+          "pitch": -0.2154119739971616,
+          "rotation": 17.27875959474387,
+          "target": "5-3-og"
+        },
+        {
+          "yaw": 2.080059843319754,
+          "pitch": -0.09690453067401705,
+          "rotation": 7.0685834705770345,
+          "target": "4-2-og"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6475936806237659,
+          "pitch": -0.1501970926776739,
+          "title": "Mountain Top",
+          "text": "<img src= images/mountain_top.png>"
+        },
+        {
+          "yaw": -1.873815981314788,
+          "pitch": -0.21496682181830806,
+          "title": "Watering Hole",
+          "text": "<img src= images/watering_hole.png>"
+        }
+      ]
+    },
+    {
+      "id": "4-2-og",
+      "name": "2. OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.74309768903397,
+          "pitch": 0.2099433899526879,
+          "rotation": 5.497787143782138,
+          "target": "3-1-og"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2456614482668087,
+          "pitch": 0.38572191799956457,
+          "title": "Cave",
+          "text": "<img src= images/cave.png>"
+        },
+        {
+          "yaw": 1.6171829169878356,
+          "pitch": -0.04969571655495386,
+          "title": "Campfire",
+          "text": "<img src= images/campfire.png>"
+        }
+      ]
+    },
+    {
+      "id": "5-3-og",
+      "name": "3. OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6666727328150053,
+          "pitch": 0.481259701834718,
+          "rotation": 6.283185307179586,
+          "target": "4-2-og"
+        },
+        {
+          "yaw": 1.2652350649931705,
+          "pitch": 0.08062818927242787,
+          "rotation": 3.141592653589793,
+          "target": "2-eg"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "TheNewLearning",
+  "name": "The New Learning",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
