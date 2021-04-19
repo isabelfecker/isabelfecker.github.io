d229457fb34071618da89a190fd885fb9ba9019e
isabelfecker <67733168+isabelfecker@users.noreply.github.com>
2021-04-20 00:07:20 +0200

    digital

--- EP/360/data.js
+++ EP/360/data.js
@@ -26,9 +26,9 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.26300283390328616,
-          "pitch": 0.15035790980708796,
-          "rotation": 27.48893571891071,
+          "yaw": -0.266001467507186,
+          "pitch": 0.09658939463546723,
+          "rotation": 33.772121026090296,
           "target": "1-01"
         }
       ],
@@ -120,12 +120,6 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.341878711116596,
-          "pitch": 0.1740743571254555,
-          "rotation": 5.497787143782138,
-          "target": "1-01"
-        },
-        {
           "yaw": 0.946127693102575,
           "pitch": -0.007460033140766598,
           "rotation": 18.84955592153877,
@@ -136,8 +130,8 @@ var APP_DATA = {
         {
           "yaw": 0.06616081979398203,
           "pitch": 0.1793484074122773,
-          "title": "<p align=\"left\" style=\"margin-bottom: 0cm; text-transform: uppercase; letter-spacing: 0.2pt; font-style: normal; font-weight: normal; line-height: 0.71cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">'Die 'Neue\nMitte'' Im Innenhof</font></font></font></p>",
-          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Die\nneue Quartiersmitte entsteht im Innenhof. </span>Hier wird sich\nbegegnet und ausgetauscht. Dieser wird von den umgebenen Gewerbe- und\nGastronomieflächen eingekreist und erhöht die Qualität des\nViertels.&nbsp;</font></font></font>\n</p>"
+          "title": "<p align=\"left\" style=\"margin-bottom: 0cm; text-transform: uppercase; letter-spacing: 0.2pt; font-style: normal; font-weight: normal; line-height: 0.71cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Die Neue Mitte Im Innenhof</font></font></font></p>",
+          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Die\nneue Quartiersmitte entsteht im Innenhof. </span>Hier wird sich\nbegegnet und ausgetauscht. Dieser wird von den umgebenen Gewerbe- und\nGastronomieflächen eingekreist und erhöht die Qualität des\nViertels.</font></font></font></p>"
         }
       ]
     },
@@ -181,22 +175,22 @@ var APP_DATA = {
           "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Treppen\nverbinden die unterschiedlichen Ebenen und ermöglichen Begegnung.</font></font></font></p>"
         },
         {
-          "yaw": -0.07700759491955722,
-          "pitch": -0.19428806375858798,
+          "yaw": -0.06721842255362098,
+          "pitch": -0.27933750433752635,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">BEGEGNUNGSZONEN</font></font></font></p>",
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#000000\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\"><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\"><span style=\"background: transparent\">In\nden Begegnungszonen</span></span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">,\ndie zum Innenhof gerichtet sind, werden die Laubengänge von den\nBewohnern </span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\"><span style=\"background: transparent\">belebt.</span></span></font></font></font></font></font></font></p>"
         },
         {
-          "yaw": -1.4807005025776938,
-          "pitch": -0.4939128967395341,
+          "yaw": -1.492781618844898,
+          "pitch": -0.4787973421069065,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">LEBEN\nIN DER HECKE</font></font></font></p>",
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">An\nStahlseilen, die zwischen den Laubengängen gespannt sind, können\nPflanzen entlang wachsen. Im Sommer bieten die bewachsenen\nRankenhilfen nicht nur Schatten und Abkühlung, sondern dienen auch\nals Rückzugsort für verschiedene Vogelarten. In der kalten\nJahreszeit fällt durch die Stahlseile noch ausreichend Licht in die\nLaubengänge.</font></font></font></p>"
         },
         {
-          "yaw": -0.5930643959563575,
-          "pitch": -0.1409098381108045,
+          "yaw": -0.5876738208597025,
+          "pitch": -0.22101842789255244,
           "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\">LAUBENGANGBEETE</span></font></font></font></font></font></p>",
-          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\"><span style=\"background: transparent\">Den\nBewohnern des Gartenhauses soll es m</span></span></font></font>öglich\nsein, ihren eigenen Garten zu betreiben. Beete auf den Laubengängen\nermöglichen den Anbau von Gemüse- und Kräuterpflanzen direkt vor\nder eigenen Haustür und lässt die Neue Mitte zu einem grün\ndurchwachsenen Ort werden.&nbsp;&nbsp;</font></font></font>\n</p>"
+          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\"><span style=\"background: transparent\">Den\nBewohnern des Gartenhauses soll es m</span></span>öglich\nsein, ihren eigenen Garten zu betreiben. Beete auf den Laubengängen\nermöglichen den Anbau von Gemüse- und Kräuterpflanzen direkt vor\nder eigenen Haustür und lässt die Neue Mitte zu einem grün\ndurchwachsenen Ort werden.</font></font></font></p>"
         },
         {
           "yaw": 0.24613487127865064,
@@ -232,8 +226,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.19869282493744223,
-          "pitch": -0.008964195124441687,
+          "yaw": -0.19111536657713302,
+          "pitch": -0.04913228904621825,
           "rotation": 7.853981633974483,
           "target": "5-05"
         }
@@ -246,8 +240,8 @@ var APP_DATA = {
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#000000\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\"><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">S</span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">eit\nAnfang der 1960</span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">er\nJahre</span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">\n</span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span lang=\"de-DE\"><span style=\"background: transparent\">ist\nder</span></span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">\nNachtwächterbrunnen </span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Teil\ndes </span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Züblin-</span></font></font></font><font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Areals.</span></font></font></font></font></font></font></p>"
         },
         {
-          "yaw": -0.5416126506932599,
-          "pitch": -0.6134517150361667,
+          "yaw": -0.5421304566519076,
+          "pitch": -0.6386405066247747,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">OFFENE\nTREPPEN</font></font></font></p>",
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Durch\noffene Treppen können die <span style=\"background: transparent\">Dachflächen\ndirekt erreicht werden.&nbsp;</span></font></font></font>\n</p>"
         }
@@ -296,19 +290,19 @@ var APP_DATA = {
           "yaw": -1.9110562981502959,
           "pitch": -0.1434758228016939,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">BEPFLANZUNG</font></font></font></p>",
-          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Die\nBepflanzung zieht sich bis zur Dachebene </span></font></font>durch\nund erinnert an das Erscheinungsbild einer Hecke.</font></font></font></p>"
+          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"background: transparent\">Die\nBepflanzung zieht sich bis zur Dachebene </span>durch und erinnert an\ndas Erscheinungsbild einer Hecke.</font></font></font></p>"
         },
         {
-          "yaw": 0.4341204508665104,
-          "pitch": 0.07622557127184315,
+          "yaw": 0.3231598521517718,
+          "pitch": 0.05063067147448663,
           "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#000000\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"text-transform: uppercase\"><font color=\"#ffffff\"><span style=\"letter-spacing: 0.4pt\">Trockenmauer&nbsp;</span></font></span></font></font></font>\n</p>",
           "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Die\nentstandenen Höhensprünge werden durch das Anlegen von\nTrockenmauern zum Versteck und Überwinterungsquartier von\nKleintieren.</font></font></font></p>"
         },
         {
-          "yaw": -0.8561768894900066,
-          "pitch": -0.8438437919278456,
-          "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\">RÜCKZUGSORT</font></font></font></p>",
-          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Maison Neue, serif\"><font style=\"font-size: 8pt\">Der\ngrüne Innenhof lockt die Vögel an.</font></font></font></p>"
+          "yaw": -0.885715303528583,
+          "pitch": -0.8764492368489147,
+          "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">RÜCKZUGSORT</font></font></font></p>",
+          "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Der\ngrüne Innenhof lockt die Vögel an.</font></font></font></p>"
         }
       ]
     },
@@ -346,14 +340,14 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": 0.06710728590799953,
-          "pitch": -0.5077630574357777,
+          "yaw": 0.06477991977009445,
+          "pitch": -0.5341695416993453,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">SPORTKÄFIG</font></font></font></p>",
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Der\nSportkäfig kann genutzt werden um darin u.a. Basketball oder\nBadminton zu spielen.</font></font></font></p>\n<p style=\"margin-bottom: 0cm; line-height: 100%\"><br>\n\n</p>"
         },
         {
-          "yaw": -2.8587920387361176,
-          "pitch": 0.24443105458415282,
+          "yaw": -2.911101165573326,
+          "pitch": 0.2783912339818535,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">FITNESS\nMIT AUSSICHT</font></font></font></p>",
           "text": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Die\nQuartiersmitte bietet auch eine Dachfläche an, auf der Sport gemacht\nwerden kann. Somit genießt man beim gemeinsamen Yoga eine Aussicht\nüber die Stadt.</font></font></font></p>"
         }
@@ -385,28 +379,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.3357698126628712,
-          "pitch": 0.45742418365237114,
+          "yaw": 0.31104482134289313,
+          "pitch": 0.45802056555585047,
           "rotation": 0,
           "target": "8-08"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.7796051192440867,
-          "pitch": -0.24423516736303696,
+          "yaw": -0.7971061574853113,
+          "pitch": -0.2825813646166999,
           "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"text-transform: uppercase\"><span style=\"letter-spacing: 0.4pt\">Dachgarten</span></span></font></font></font></p>",
           "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Im\nöffentlichen Stadtgarten auf dem Dach gibt es auch für externe\nGärtner die Möglichkeit aktiv zu werden. Auf Vereinsbasis werden\ndie Beete untereinander aufgeteilt und gepflegt.</font></font></font></p>"
         },
         {
-          "yaw": -2.2289704800478294,
-          "pitch": -0.5724910895908142,
+          "yaw": -2.2556164118124915,
+          "pitch": -0.6358472551690344,
           "title": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">BRUTPLATZ</font></font></font></p>",
           "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Viele\nVogelarten werden von der Hecke angezogen. Hier können sie brüten\nund sich zurückziehen.</font></font></font></p>"
         },
         {
-          "yaw": -0.3930136224848404,
-          "pitch": -0.19480547463812314,
+          "yaw": -0.3929936653672481,
+          "pitch": -0.21263414697339655,
           "title": "<p align=\"justify\" style=\"margin-bottom: 0cm; font-style: normal; font-weight: normal; line-height: 0.35cm;\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\"><span style=\"text-transform: uppercase\"><span style=\"letter-spacing: 0.4pt\">Wintergarten</span></span></font></font></font></p>",
           "text": "<p style=\"margin-bottom: 0cm; line-height: 100%\">\n<font color=\"#ffffff\"><font face=\"Arial, sans-serif\"><font style=\"font-size: 9pt\">Durch\nden Gashauseffekt innerhalb der Wintergärten ist es möglich, auch\nohne weitere Wärmequelle die Temperatur so zu halten, dass geeignete\nPflanzen dort überwintern können.</font></font></font></p>"
         }
@@ -438,8 +432,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.8221396981250209,
-          "pitch": 0.782711501025025,
+          "yaw": 0.7600504315923935,
+          "pitch": 0.7591614008173444,
           "rotation": 29.845130209103058,
           "target": "1-01"
         },
